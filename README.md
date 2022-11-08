f42ac9eaf047c1762ed05a9ae5797c7c11e7ccbc
AdityaKhowalGithub <adityakh2003@outlook.com>
2022-11-07 17:06:53 -0800

    h

---
+++ README.md
@@ -0,0 +1,4 @@
+sun setup.py before starting!
+
+the main dependency that has issues is fbprophet. When using an arm computer you must run the terminal through rosetta! Otherwise neuralprophet is a good alternative
+
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from datetime import date
 import yfinance as yf
-from neuralprophet import NeuralProphet
-from prophet.plot import plot_plotly
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 START = "2020-01-01"#start of stock data
@@ -27,9 +27,14 @@ def load_data(ticker):
 data_load_state = st.text("Load data ...")
 data = load_data(selected_stocks)
 data_load_state.text("Loading data ... Done!")
+def view():
+    st.subheader('Raw data')
+    st.write(data.tail())
 
-st.subheader('Raw data')
-st.write(data.tail())
+st.checkbox("View Raw", value=True, on_change=view())
+
+
+
 
 def plot_raw_data():
     fig = go.Figure()
@@ -49,30 +54,49 @@ plot_raw_data()
 df_train = data[['Date','Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-m = NeuralProphet()
-metrics = m.fit(df_train, freq = "D")
-future = m.make_future_dataframe(df_train, periods=period)
+
+#================================================================#
+# m = NeuralProphet()
+# metrics = m.fit(df_train, freq = "D")
+# future = m.make_future_dataframe(df_train, periods=period)
+# forecast = m.predict(future)
+
+# # Show and plot forecast
+# st.subheader('Forecast data')
+# #st.write(forecast.tail())
+
+# # create forecast
+# df_future = m.make_future_dataframe(df_train, periods=365)
+# forecast = m.predict(df_future)
+# # create plots
+
+# fig_forecast = m.plot(forecast)
+# fig_components = m.plot_components(forecast)
+# #fig_model = m.plot_parameters()
+
+# st.write(f'Forecast plot for {n_years} years')
+# #fig1 = plot_plotly(fig_forecast)
+# st.plotly_chart(fig_forecast)
+
+# st.write("Forecast components")
+# fig2 = m.plot_components(fig_components)
+# st.write(fig2)
+
+#================================================================#
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 
 # Show and plot forecast
 st.subheader('Forecast data')
-#st.write(forecast.tail())
-
-# create forecast
-df_future = m.make_future_dataframe(df_train, periods=365)
-forecast = m.predict(df_future)
-# create plots
-
-fig_forecast = m.plot(forecast)
-fig_components = m.plot_components(forecast)
-#fig_model = m.plot_parameters()
-
+st.write(forecast.tail())
+    
 st.write(f'Forecast plot for {n_years} years')
-#fig1 = plot_plotly(fig_forecast)
-st.plotly_chart(fig_forecast)
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
 
 st.write("Forecast components")
-fig2 = m.plot_components(fig_components)
+fig2 = m.plot_components(forecast)
 st.write(fig2)
-
-
